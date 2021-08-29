d91ab01ec254262b8ef98012dded62e242a61b34
stephanieraymos <stephanieraymos@gmail.com>
2021-08-28 22:29:44 -0700

    Created geometry (square) and material (red)

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const scene = new THREE.Scene()
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1); // Params are size
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
