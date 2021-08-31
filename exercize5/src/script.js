8eee3eb8030b357eea553e8ae61a87e4a1b8c635
stephanieraymos <stephanieraymos@gmail.com>
2021-08-30 22:26:38 -0700

    3 cubes

--- exercize5/src/script.js
+++ exercize5/src/script.js
@@ -7,15 +7,26 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(0.7, -0.6, 1) // Setting x, y and z shorthand
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
 
-mesh.scale.set(2, 0.5, 0.5)
-
-
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xffff00})
+)
+cube2.position.x = - 2
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ffff})
+)
+cube3.position.x = 2
+group.add(cube3)
 
 const sizes = {
     width: 800,
@@ -25,9 +36,6 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
-
-camera.lookAt(new THREE.Vector3(3, 0, 0))
-
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
