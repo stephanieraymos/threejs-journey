41c7f4a355539e55b68e0dc6a3a518f18188771d
stephanieraymos <stephanieraymos@gmail.com>
2021-08-30 22:20:15 -0700

    Camera looking right

--- exercize5/src/script.js
+++ exercize5/src/script.js
@@ -26,7 +26,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-camera.lookAt(mesh.position)
+camera.lookAt(new THREE.Vector3(3, 0, 0))
 
 
 const renderer = new THREE.WebGLRenderer({
