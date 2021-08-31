c5b16e7c7d0188c1a3ca664c2671fb83e06bdce1
stephanieraymos <stephanieraymos@gmail.com>
2021-08-30 21:55:37 -0700

    Position shorthand (set)

--- exercize5/src/script.js
+++ exercize5/src/script.js
@@ -10,9 +10,9 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.7
-mesh.position.y = 0.6
-mesh.position.z = 1
+
+mesh.position.set(0.7, -0.6, 1) // Setting x, y and z shorthand
+
 scene.add(mesh)
 
 const sizes = {
@@ -23,6 +23,7 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
