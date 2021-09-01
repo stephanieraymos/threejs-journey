b88c35793e127adda30cb16a61e424ed73b718ce
stephanieraymos <stephanieraymos@gmail.com>
2021-08-31 22:10:09 -0700

    Left clock for reference

--- exercize6/src/script.js
+++ exercize6/src/script.js
@@ -31,12 +31,21 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 }); 
-gsap.to(mesh.position, { duration: 1, delay: 2, x: -2 }); 
+// Clock
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: -2 });
 //1. Object to animate
 //2. Options
 // animations
 const tick = () => {
+  // Clock
+  //   const elapsedTime = clock.getElapsedTime();
+  //   //Update Objects
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
