86cf3d0bec8b47288f22886346025c85bb331bd5
stephanieraymos <stephanieraymos@gmail.com>
2021-09-05 13:20:06 -0700

    Mesh position

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -90,9 +90,9 @@ const tick = () => {
   //|------------------------|
   //|----- Update camera ----|
   //|------------------------|
-  camera.position.x = cursor.x * 3
-  camera.position.y = cursor.y * 3
-  camera.lookAt(new THREE.Vector3())
+  camera.position.x = cursor.x * 10
+  camera.position.y = cursor.y * 10
+  camera.lookAt(mesh.position)
 
   // Render
   renderer.render(scene, camera);
