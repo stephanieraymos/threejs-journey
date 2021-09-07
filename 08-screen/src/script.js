af20e72ad3f9cc230d8ac3fc1efdc87116f5ea21
stephanieraymos <stephanieraymos@gmail.com>
2021-09-06 22:26:34 -0700

    Double click for full screen and exit working

--- 08-screen/src/script.js
+++ 08-screen/src/script.js
@@ -43,9 +43,9 @@ window.addEventListener("resize", () => {
 
 window.addEventListener("dblclick", () => {
   if (!document.fullscreenElement) {
-    console.log("Go fullscreen");
+    canvas.requestFullscreen();
   } else {
-    console.log("Leave fullscreen");
+    document.exitFullscreen();
   }
 });
 
