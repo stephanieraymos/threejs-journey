269215351431977fd5071e5a35a20688ccf8fb13
stephanieraymos <stephanieraymos@gmail.com>
2021-09-07 22:27:10 -0700

    Positions array

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -12,7 +12,10 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const positionsArray = new Float32Array(9);
+
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
