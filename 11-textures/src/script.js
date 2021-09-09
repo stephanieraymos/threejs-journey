4c87b788f525a87b1ff76c0c30b371b340c49013
stephanieraymos <stephanieraymos@gmail.com>
2021-09-09 12:52:58 -0700

    Loading

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -4,7 +4,18 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Textures
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load("/textures/door/color.jpg");
+const texture = textureLoader.load(
+  "/textures/door/color.jpg",
+  () => {
+    console.log("Load");
+  },
+  () => {
+    console.log("Progress");
+  },
+  () => {
+    console.log("Error");
+  }
+);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
