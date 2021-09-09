5e9f10b3d58fa23494343eecc39cc427a0d08b47
stephanieraymos <stephanieraymos@gmail.com>
2021-09-09 12:48:01 -0700

    Setting texture for material

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -26,7 +26,7 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ map: texture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
