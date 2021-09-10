1bbd698fb4bbf3772e3c8e2cdbd36d3a82128fc1
stephanieraymos <stephanieraymos@gmail.com>
2021-09-09 21:56:39 -0700

    Learning new ways of doing things. Mapping texture to material outside

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -26,7 +26,9 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // objects
-const material = new THREE.MeshBasicMaterial({ map: doorTexture });
+const material = new THREE.MeshBasicMaterial();
+material.map = doorTexture;
+
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
 
