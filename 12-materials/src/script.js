5a641810c30124cd742d2b13eb41c18285a5c325
stephanieraymos <stephanieraymos@gmail.com>
2021-09-09 21:43:27 -0700

    X axis rotation, a teeeeeeny bit faster

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -87,6 +87,9 @@ const tick = () => {
   sphere.rotation.y = 0.1 * elapsedTime;
   plane.rotation.y = 0.1 * elapsedTime;
   torus.rotation.y = 0.1 * elapsedTime;
+  sphere.rotation.x = 0.15 * elapsedTime;
+  plane.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.x = 0.15 * elapsedTime;
 
   // Update controls
   controls.update();
