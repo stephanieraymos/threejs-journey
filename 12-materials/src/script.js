b4619a619961ad4b6d3108ba822629b5e888c230
stephanieraymos <stephanieraymos@gmail.com>
2021-09-09 21:42:36 -0700

    Slow rotation

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -83,6 +83,11 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // Update object
+  sphere.rotation.y = 0.1 * elapsedTime;
+  plane.rotation.y = 0.1 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+
   // Update controls
   controls.update();
 
