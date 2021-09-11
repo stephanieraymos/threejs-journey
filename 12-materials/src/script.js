6857852ac592f92e7dc7433dadb27335edce8dd3
stephanieraymos <stephanieraymos@gmail.com>
2021-09-10 22:30:39 -0700

    Lambert material with ambient and point lights

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -43,7 +43,10 @@ const scene = new THREE.Scene();
 // const material = new THREE.MeshMatcapMaterial();
 // material.matcap = matcapTexture;
 
-const material = new THREE.MeshDepthMaterial();
+// White when close, black when far
+// const material = new THREE.MeshDepthMaterial();
+
+const material = new THREE.MeshLambertMaterial();
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
@@ -57,6 +60,15 @@ const torus = new THREE.Mesh(
 torus.position.x = 1.5;
 
 scene.add(sphere, plane, torus);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
 /**
  * Sizes
  */
