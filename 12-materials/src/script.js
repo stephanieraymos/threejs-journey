825e74266a85d915cf2560f4d24e282b3455afe0
stephanieraymos <stephanieraymos@gmail.com>
2021-09-12 22:12:06 -0700

    Shininess

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -48,6 +48,11 @@ const scene = new THREE.Scene();
 
 // const material = new THREE.MeshLambertMaterial();
 const material = new THREE.MeshPhongMaterial();
+material.shininess = 100
+material.specular = new THREE.Color("grey");
+// const material = new THREE.Mesh
+
+
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
 
