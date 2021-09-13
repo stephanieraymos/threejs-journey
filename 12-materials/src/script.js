c9b9264b7f260e50543d92c450e8873fbe17470c
stephanieraymos <stephanieraymos@gmail.com>
2021-09-12 22:27:21 -0700

    Adding tweaks

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -63,6 +63,9 @@ const material = new THREE.MeshStandardMaterial();
 material.metalness = 0.45;
 material.roughness = 0.65;
 
+gui.add(material, "metalness").min(0).max(1).step(0.0001);
+gui.add(material, "roughness").min(0).max(1).step(0.0001);
+
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
 
