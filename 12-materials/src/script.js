75823e0dfa658713c970a172559f85591ff407f5
stephanieraymos <stephanieraymos@gmail.com>
2021-09-12 22:38:56 -0700

    Ambient occlusion

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -63,21 +63,33 @@ const material = new THREE.MeshStandardMaterial();
 material.metalness = 0.45;
 material.roughness = 0.65;
 material.map = doorTexture;
+material.aoMap = ambientTexture;
 
 gui.add(material, "metalness").min(0).max(1).step(0.0001);
 gui.add(material, "roughness").min(0).max(1).step(0.0001);
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
+sphere.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+);
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-console.log(plane.geometry);
+plane.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+);
 
 const torus = new THREE.Mesh(
   new THREE.TorusGeometry(0.5, 0.2, 16, 32),
   material
 );
 torus.position.x = 1.5;
+torus.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
+);
 
 scene.add(sphere, plane, torus);
 
