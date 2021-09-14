b87fcbad7c0545b9248979ec45bdb169c13fb121
stephanieraymos <stephanieraymos@gmail.com>
2021-09-13 20:55:49 -0700

    Added displacement scale to panel

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -72,6 +72,7 @@ material.displacementScale = 0.05;
 
 gui.add(material, "metalness").min(0).max(1).step(0.0001);
 gui.add(material, "roughness").min(0).max(1).step(0.0001);
+gui.add(material, "displacementScale").min(0).max(1).step(0.0001);
 gui.add(material, "aoMapIntensity", 0, 10).step(0.01);
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
