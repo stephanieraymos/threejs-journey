9b6211834102faba47cd64c236c6b983d467fc13
stephanieraymos <stephanieraymos@gmail.com>
2021-09-17 15:24:03 -0700

    Text geometry and text material

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -23,9 +23,19 @@ const textureLoader = new THREE.TextureLoader();
 // FONTS
 const fontLoader = new THREE.FontLoader();
 
-fontLoader.load("./fonts/helvetiker_regular.typeface.json", () => {
-    console.log("font loaded");
-});
+fontLoader.load("./fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeo = new THREE.TextGeometry("Stephanie Raymos", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const textMaterial = new THREE.MeshBasicMaterial();
 
 /**
  * Object
